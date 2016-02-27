eefa93a0ab18ffc379caa65586b8686c5bfede9a
Jamie Sykes <jaymehsykes@gmail.com>
2016-02-27 15:44:10 +0000

    Addition of Imagemin Functionality for Assets

--- GruntFile.js
+++ GruntFile.js
@@ -4,6 +4,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-open');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
@@ -32,6 +33,39 @@ module.exports = function (grunt) {
                 dest: 'deploy/js/<%= pkg.name %>.min.js'
             }
         },
+        imagemin: {
+            png: {
+                options: {
+                    optimizationLevel: 3
+                },
+                files: [{
+                    // Set to true to enable the following options…
+                    expand: true,
+                    // cwd is 'current working directory'
+                    cwd: 'src/assets/',
+                    src: ['*.png'],
+                    // Could also match cwd line above. i.e. project-directory/img/
+                    dest: 'deploy/assets/',
+                    ext: '.png'
+                }
+              ]
+            },
+            jpg: {
+                options: {
+                    progressive: true
+                },
+                files: [{
+                    // Set to true to enable the following options…
+                    expand: true,
+                    // cwd is 'current working directory'
+                    cwd: 'src/assets/',
+                    src: ['**/*.jpg'],
+                    // Could also match cwd. i.e. project-directory/img/
+                    dest: 'deploy/assets/',
+                    ext: '.jpg'
+                }]
+            }
+        },
         watch: {
             files: 'src/**/*.js',
             tasks: ['concat']
@@ -45,5 +79,6 @@ module.exports = function (grunt) {
 
     grunt.registerTask('default', ['concat', 'connect', 'open', 'watch']);
     grunt.registerTask('compress', ['uglify']);
+    grunt.registerTask('min', ['imagemin']);
 
 }
--- deploy/js/game.js
+++ deploy/js/game.js
@@ -30,12 +30,6 @@ var RPG = RPG || {};
 RPG.GameState = {
     init: function() {
         // Delete this init block or replace with your own logic.
-
-        // Create simple text display for current Phaser version
-        /*var text = "Phaser Version "+Phaser.VERSION + " works!";
-        var style = { font: "24px Arial", fill: "#fff", align: "center" };
-        var t = game.add.text(this.world.centerX, this.world.centerY, text, style);
-        t.anchor.setTo(0.5, 0.5); */
         dialog.init(60, 50, {'extendedBackground': true});
 
     },
